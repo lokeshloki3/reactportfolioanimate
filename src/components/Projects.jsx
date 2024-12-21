61454fa5719143712df87a736b184ed473ba8bfb
lokeshloki3 <lokesh1992p@gmail.com>
2024-12-21 14:16:01 +0530

    mobile responsive

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,28 +1,16 @@
 import React from "react";
 import { PROJECTS } from "../constants";
-import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
     <div className="pb-4">
-      <motion.h2
-        whileInView={{ opacity: 1, y: 0 }}
-        initial={{ opacity: 0, y: -100 }}
-        transition={{ duration: 0.5 }}
-        className="my-20 text-center text-4xl"
-        id="projects"
-      >
+      <h2 className="my-20 text-center text-4xl" id="projects">
         Projects
-      </motion.h2>
+      </h2>
       <div>
         {PROJECTS.map((project, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: -100 }}
-              transition={{ duration: 1 }}
-              className="w-full lg:w-1/4"
-            >
+            <div className="w-full lg:w-1/4">
               <a href={project.githubPage} target="_blank">
                 <img
                   src={project.image}
@@ -32,28 +20,23 @@ const Projects = () => {
                   className="mb-6 rounded"
                 />
               </a>
-            </motion.div>
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: -100 }}
-              transition={{ duration: 1 }}
-              className="w-full max-w-xl lg:w-3/4"
-            >
+            </div>
+            <div className="w-full max-w-xl lg:w-3/4">
               <a href={project.githubPage} target="_blank">
                 <h3 className="mb-2 font-semibold text-2xl">{project.title}</h3>
               </a>
               <p className="mb-4 text-stone-400">{project.description}</p>
-              <p className="grid grid-cols-3 sm:grid-cols-4 gap-2">
+              <p className="grid grid-cols-3 sm:grid-cols-5 gap-2">
                 {project.technologies.map((tech, index) => (
                   <span
-                    className="mr-2 rounded bg-stone-900 p-2 text-sm font-medium text-stone-300"
+                    className="rounded bg-stone-900 p-2 text-sm font-medium text-stone-300"
                     key={index}
                   >
                     {tech}
                   </span>
                 ))}
               </p>
-            </motion.div>
+            </div>
           </div>
         ))}
       </div>
