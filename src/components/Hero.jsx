250f6b7bba2b9fe1b0e726783134627c51e34faf
lokesh <lokesh199p@gmail.com>
2026-04-21 23:24:51 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -62,7 +62,7 @@ const Hero = () => {
               variants={childVariants}
               className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent"
             >
-              Frontend Developer
+              Software Engineer
             </motion.span>
             <motion.p
               variants={childVariants}
